832e045e244c6bbe24b4614debc6974107bde1a1
siquel <squual@kahvipaussi.net>
2015-02-21 03:39:19 +0200

    Remove newline from STDOUT

--- SalattuImuri/Rsync.pm
+++ SalattuImuri/Rsync.pm
@@ -22,15 +22,14 @@ sub vacuum {
 		foreach my $filter (@{$self->{'config'}->getFilters()}) {
 			my $re = "$filter->{'regex'}$filter->{'resolution'}.+$filter->{'source'}";
 			my @matches = grep(/$re/i, @files);
-			print @matches;
+			$self->initiateVacuuming($filter, @matches);
 		}	
 	}
 }
 
-sub processFilters {
-	my ($self, $filters) = @_;
-#	foreach my $filter (
-	
+sub initiateVacuuming {
+	my ($self, $filter, @files) = @_;
+	print join(' ', @files);	
 }
 
 sub asd {
@@ -43,7 +42,11 @@ sub listFiles {
 	my ($self, $server) = @_;
 	open(my $rsync_h, '-|', "rsync --list-only -e ssh $server->{'username'}"."@"."$server->{'hostname'}:$server->{'root'} ".
 		'| awk \'{$1=$2=$3=$4=""; print substr($0,5); }\'') or die $!;
-	my @files = <$rsync_h>;
+	my @files;
+	while ( <$rsync_h>) {	
+		chomp;
+		push @files, $_;
+	}
 	close($rsync_h);
 	return @files;
 }
